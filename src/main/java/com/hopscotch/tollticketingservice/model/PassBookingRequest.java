046fb7e64c9b6b10303be238324b7ecd2aa750c3
Manoj Prasad Singh <YATRAONLINE\manoj.singh@lap0002517.yatraonline.local>
2020-10-03 20:51:12 +0530

    pass bokk api tested

--- src/main/java/com/hopscotch/tollticketingservice/model/PassBookingRequest.java
+++ src/main/java/com/hopscotch/tollticketingservice/model/PassBookingRequest.java
@@ -1,24 +1,31 @@
 package com.hopscotch.tollticketingservice.model;
-import com.fasterxml.jackson.annotation.JsonInclude;
 import lombok.Data;
-import javax.validation.constraints.NotEmpty;
-import javax.validation.constraints.NotNull;
+
+import javax.validation.constraints.*;
+
 @Data
-@JsonInclude(JsonInclude.Include.NON_NULL)
 public class PassBookingRequest {
+    @Positive(message = "Invalid TollId ")
     private Long tollId;
+    @Positive(message = "Invalid BoothId ")
     private Long boothId;
+    @Positive(message = "passPrice should be positive value")
+    @Digits(integer = 10, fraction = 2,message = "passPrice is invalid")
     private Double passPrice;
     @NotNull(message = "VehicleRegNum can't be null")
     @NotEmpty(message = "VehicleRegNum can't be empty")
+    @NotBlank(message = "VehicleRegNum can't be Blank")
     private String vehicleRegNum;
     @NotNull(message = "VehicleType can't be null")
     @NotEmpty(message = "VehicleType can't be empty")
+    @NotBlank(message = "VehicleType can't be Blank")
     private String vehicleType;
     @NotNull(message = "PassType can't be null")
     @NotEmpty(message = "PassType can't be empty")
+    @NotBlank(message = "PassType can't be Blank")
     private String passType;
     @NotNull(message = "TollName can't be null")
     @NotEmpty(message = "TollName can't be empty")
+    @NotBlank(message = "TollName can't be Blank")
     private String tollName;
 }
